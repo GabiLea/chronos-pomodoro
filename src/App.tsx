113167d9135a23f1a54d20590482ba8712023baf
Gabi Leandro <gabriela.leandro.ti@gmail.com>
2025-12-21 12:44:15 -0300

    Curso

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,59 @@
 import './styles/theme.css'
 import './styles/global.css'
 
+import { Container } from './components/Container'
+import { Menu } from './components/Menu'
+import { Logo } from './components/Logo'
+import { CountingDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
+import { Cycles } from './components/Cycles'
+import { DefaultButton } from './components/DefaultButton'
+import { PlayCircleIcon } from 'lucide-react'
+import { Footer } from './components/Footer'
+
 export function App() {
   console.log('app component')
   return (
     <>
-      <h1>olá mundo</h1>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountingDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+      <Container>
+        <Footer />
+      </Container>
     </>
   )
 }
---
+++ src/components/CountDown/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export function CountingDown() {
+	return (
+		<div className={styles.container}>
+			00:00
+		</div>
+	)
+}
